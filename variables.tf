94ccf3b21c816d1f8a44f317888096bda33c82bc
pranayentc <109445871+pranayentc@users.noreply.github.com>
2025-04-15 18:58:53 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  
+}
+
+variable "instance_count" {
+
+}
+
+variable "ami_id" {
+  
+}
+
+variable "key" {
+  
+}
+
+variable "itype" {
+  
+}
+
+variable "iname" {
+  
+}
+
+
